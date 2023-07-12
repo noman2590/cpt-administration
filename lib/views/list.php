08940fc0f0aa410b1382965f53c585705fef169b
Noman Akram <nomanakram2590@gmail.com>
2023-07-12 15:05:59 +0500

    add cpt button

--- lib/views/list.php
+++ lib/views/list.php
@@ -6,58 +6,61 @@
     unset($_SESSION['cpta_form_fail']);
 ?>
 <div class="wrap cpta-listing-page">
-    <h1 class="wp-heading-inline">CPT Listing</h1>
-    <a href="<?php echo admin_url()?>admin.php?page=add-new-cpt" class="page-title-action">Add New Post Type</a>
-    <div class="alert cpta-alert-success <?php echo ($form_success) ? '' : 'cpta-hidden' ?>">
-        <?php echo ($form_success) ? $form_success : '' ?>
-    </div>
-    <div class="alert cpta-alert-fail <?php echo ($form_fail) ? '' : 'cpta-hidden' ?>">
-        <?php echo ($form_fail) ? $form_fail : '' ?>
-    </div>
-	<div class="bg-white cpta-list-box">
-		<div class="ai1wm-left">
-            <table id="myTable" class="striped widefat">
-                <thead>
-                <tr>
-                    <th>Slug</th>
-                    <th>Singular Label</th>
-                    <th>Plural Label</th>
-                    <th>Menu Position</th>
-                    <th>Menu Icon</th>
-                    <th>Action</th>
-                 </tr>
-                </thead>
-                <tbody>
-                    <?php 
-                    if(count($data['data'])){
-                    foreach ($data['data'] as $key=>$value){ 
-                    ?>
-                    <tr>
-                        <td><?php echo $value->slug ?></td>
-                        <td><?php echo $value->singular ?></td>
-                        <td><?php echo $value->plural ?></td>
-                        <td><?php echo $value->menu_position ?></td>
-                        <td><i class="dashicons <?php echo $value->menu_icon ?>"></i></td>
-                        <td class="action">
-                            <a href="<?php echo admin_url()?>admin.php?page=edit-cpt&id=<?php echo $value->id ?>"><i class="dashicons dashicons-edit"></i></a>
-                            <form method="post" action="<?php echo esc_url(admin_url('admin-post.php')); ?>">
-                                <i class="dashicons dashicons-trash cpta-delete-btn" title="Delete post type"></i>
-                                <input type="hidden" name="action" value="delete_custom_posttype">
-                                <input type="hidden" name="id" value="<?php echo $value->id; ?>">
-                                <input type="hidden" name="slug" value="<?php echo $value->slug; ?>">
-                                <?php wp_nonce_field( 'delete_custom_posttype_action', 'delete_custom_posttype_nonce' ); ?>
-                            </form>
-                        </td>
-                    </tr>
-                    <?php }}else { ?>
-                    <tr>
-                        <td colspan="6">
-                            <p style="text-align:center">No post type found</p>
-                        </td>
-                    </tr>
-                    <?php } ?>
-                </tbody>
-            </table>
+    <div class="wpbody-content">
+        <h1 class="wp-heading-inline">CPT Listing</h1>
+        <a href="<?php echo admin_url()?>admin.php?page=add-new-cpt" class="page-title-action">Add New Post Type</a>
+        <hr class="wp-header-end" />
+        <div class="alert cpta-alert-success <?php echo ($form_success) ? '' : 'cpta-hidden' ?>">
+            <?php echo ($form_success) ? $form_success : '' ?>
         </div>
-	</div>
+        <div class="alert cpta-alert-fail <?php echo ($form_fail) ? '' : 'cpta-hidden' ?>">
+            <?php echo ($form_fail) ? $form_fail : '' ?>
+        </div>
+        <div class="bg-white cpta-list-box">
+            <div class="ai1wm-left">
+                <table id="myTable" class="striped widefat">
+                    <thead>
+                    <tr>
+                        <th>Slug</th>
+                        <th>Singular Label</th>
+                        <th>Plural Label</th>
+                        <th>Menu Position</th>
+                        <th>Menu Icon</th>
+                        <th>Action</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        <?php 
+                        if(count($data['data'])){
+                        foreach ($data['data'] as $key=>$value){ 
+                        ?>
+                        <tr>
+                            <td><?php echo $value->slug ?></td>
+                            <td><?php echo $value->singular ?></td>
+                            <td><?php echo $value->plural ?></td>
+                            <td><?php echo $value->menu_position ?></td>
+                            <td><i class="dashicons <?php echo $value->menu_icon ?>"></i></td>
+                            <td class="action">
+                                <a href="<?php echo admin_url()?>admin.php?page=edit-cpt&id=<?php echo $value->id ?>"><i class="dashicons dashicons-edit"></i></a>
+                                <form method="post" action="<?php echo esc_url(admin_url('admin-post.php')); ?>">
+                                    <i class="dashicons dashicons-trash cpta-delete-btn" title="Delete post type"></i>
+                                    <input type="hidden" name="action" value="delete_custom_posttype">
+                                    <input type="hidden" name="id" value="<?php echo $value->id; ?>">
+                                    <input type="hidden" name="slug" value="<?php echo $value->slug; ?>">
+                                    <?php wp_nonce_field( 'delete_custom_posttype_action', 'delete_custom_posttype_nonce' ); ?>
+                                </form>
+                            </td>
+                        </tr>
+                        <?php }}else { ?>
+                        <tr>
+                            <td colspan="6">
+                                <p style="text-align:center">No post type found</p>
+                            </td>
+                        </tr>
+                        <?php } ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 </div>
